e80df6f945e664c256eca783eabd4474dc7b08b2
JesseBot <jessebot@linux.com>
2022-08-09 13:20:23 +0200

    added new bash functions

--- k3s/quick-start-k3s.sh
+++ k3s/quick-start-k3s.sh
@@ -13,7 +13,25 @@
 #       CREATED: 04/08/2022 22:24:00
 #
 #===============================================================================
+
 source .env
+
+# etremely simply loading bar
+function simple_loading_bar() {
+    echo ""
+    for i in $(seq 1 $1); do
+        echo -n "   ¯\_(ツ)_/¯";
+        sleep 1
+    done
+    echo ""
+}
+
+# pretty echo so that I don't have ot remember this incantation
+function p_echo() {
+    echo ""
+    echo -e "\033[92m  $1 \033[00m"
+    echo ""
+}
 
 # skip install of traefik & servicelb, specify flannel backend
 # export INSTALL_K3S_EXEC=" --no-deploy servicelb --no-deploy traefik --flannel-backend host-gw"
@@ -22,32 +40,32 @@ export INSTALL_K3S_EXEC=" --no-deploy servicelb --no-deploy traefik"
 export K3S_KUBECONFIG_MODE="644"
 
 # create the k3s cluster (just one server node)
-echo -e "\033[92m curl -sfL https://get.k3s.io | sh - \033[00m"
+p_echo "curl -sfL https://get.k3s.io | sh -"
 curl -sfL https://get.k3s.io | sh -
 
 # Grab the kubeconfig
-echo -e "\033[92m cp the k3s kubeconfig to the right place \033[00m"
+p_echo "cp the k3s kubeconfig to the right place"
 mkdir -p ~/.kube
 cp /etc/rancher/k3s/k3s.yaml ~/.kube/kubeconfig
 # change the permissions os that it doesn't complain
 chmod 600 ~/.kube/kubeconfig
 
 # add/update all relevant helm repos
-echo -e "\033[92m Add/update helm repos for metallb, ingress-nginx, and cert-manager.\033[00m"
+p_echo "Add/update helm repos for metallb, ingress-nginx, and cert-manager."
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add metallb https://metallb.github.io/metallb
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 
 # metal lb installation: https://github.com/metallb/metallb/tree/main/charts/metallb
-echo -e "\033[92m helm install metallb metallb/metallb -n kube-system \033[00m"
+p_echo "helm install metallb metallb/metallb -n kube-system"
 helm install metallb metallb/metallb -n kube-system
 
 # wait on metallb to deploy
-echo -e "\033[92m kubectl rollout status -n kube-system deployment/metallb-controller \033[00m"
+p_echo "kubectl rollout status -n kube-system deployment/metallb-controller"
 kubectl rollout status -n kube-system deployment/metallb-controller
 
-echo -e "\033[92m Waiting on metallb controller and speaker, just to be sure \033[00m"
+p_echo "Waiting on metallb controller and speaker, just to be sure"
 kubectl wait --namespace kube-system \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/instance=metallb \
@@ -59,40 +77,42 @@ kubectl wait --namespace kube-system \
   --timeout=120s
 
 # metallb custom resources.... https://metallb.universe.tf/configuration/
-echo -e "\033[92m Applying metallb custom resources \033[00m"
-apply_exit_code=1
-echo -e "\033[92m while looping on metallb CR applying... \033[00m"
+p_echo "Applying metallb custom resources"
+kubectl apply -f metallb_cr.yml
+apply_exit_code=$?
 while [ $apply_exit_code -ne 0 ]; do
-    sleep 2
-    echo -e "\033[92m Trying to do a kube apply on this metallb CR again...\033[00m"
+    p_echo "Kube apply failed. Will loop on applying the metallb CR..."
+    p_echo "Sleeping 3 seconds before trying again."
+    simple_loading_bar 3
+    p_echo "kubectl apply -f metallb_cr.yml"
     kubectl apply -f metallb_cr.yml
     apply_exit_code=$?
 done
 
 # installing nginx
-echo -e "\033[92m helm install nginx-ingress ingress-nginx/ingress-nginx --namespace kube-system \033[00m"
+p_echo "helm install nginx-ingress ingress-nginx/ingress-nginx --namespace kube-system"
 helm install nginx-ingress ingress-nginx/ingress-nginx --namespace kube-system
 
 # wait on nginx ingress controller to deploy
-echo -e "\033[92m kubectl rollout status -n kube-system deployment/nginx-ingress-ingress-nginx-controller \033[00m"
+p_echo "kubectl rollout status -n kube-system deployment/nginx-ingress-ingress-nginx-controller"
 kubectl rollout status -n kube-system deployment/nginx-ingress-ingress-nginx-controller
 
-echo -e "\033[92m waiting on nginx \033[00m"
+p_echo "waiting on nginx"
 kubectl wait --namespace kube-system \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/name=ingress-nginx \
   --timeout=90s
 
 # Set up cert manager
-echo -e "\033[92m helm install cert-manager jetstack/cert-manager --namespace kube-system --version v1.9.1 --set installCRDs=true --set cert-manager_values.yml \033[00m"
+p_echo "helm install cert-manager jetstack/cert-manager --namespace kube-system --version v1.9.1 --set installCRDs=true --set cert-manager_values.yml"
 helm install cert-manager jetstack/cert-manager --namespace kube-system --version v1.9.1 --set installCRDs=true --set podDnsPolicy="None" --values cert-manager_values.yml
 
 # wait on cert-manager to deploy
-echo -e "\033[92m kubectl rollout status -n kube-system deployment/cert-manager \033[00m"
+p_echo "kubectl rollout status -n kube-system deployment/cert-manager"
 kubectl rollout status -n kube-system deployment/cert-manager
 kubectl rollout status -n kube-system deployment/cert-manager-webhook
 
-echo -e "\033[92m waiting on cert-manager \033[00m"
+p_echo "waiting on cert-manager"
 kubectl wait --namespace kube-system \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/name=cert-manager \
@@ -102,12 +122,12 @@ kubectl wait --namespace kube-system \
   --selector=app.kubernetes.io/component=webhook \
   --timeout=90s
 
-echo -e "\033[92m Installing clusterissuer resource for cert manager to work \033[00m"
-echo -e "\033[92m while looping on applying cluster issue CR... \033[00m"
+p_echo "Installing clusterissuer resource for cert manager to work"
+p_echo "while looping on applying cluster issue CR..."
 cert_manager_apply_exit_code=1
 while [ $cert_manager_apply_exit_code -ne 0 ]; do
-    sleep 2
-    echo -e "\033[92m Trying to do a kube apply on this clusterissuer CR for cert-manager again...\033[00m"
+    simple_loading_bar 2
+    p_echo "Trying to do a kube apply on this clusterissuer CR for cert-manager..."
     cat <<EOF | kubectl apply -f -
     apiVersion: cert-manager.io/v1
     kind: ClusterIssuer
@@ -126,8 +146,6 @@ while [ $cert_manager_apply_exit_code -ne 0 ]; do
 EOF
     cert_manager_apply_exit_code=$?
 done
-
-sleep 2
 
 if [ "$1" == "--no-k9s" ]; then
     echo "We're all done!"
