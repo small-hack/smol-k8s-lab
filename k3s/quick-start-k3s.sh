5b8796e2a8132d4557e3faa748c299c949f69d96
JesseBot <jessebot@linux.com>
2022-08-10 16:27:13 +0200

    removing grafana repo

--- k3s/quick-start-k3s.sh
+++ k3s/quick-start-k3s.sh
@@ -58,7 +58,6 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add metallb https://metallb.github.io/metallb
 helm repo add jetstack https://charts.jetstack.io
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # metal lb installation: https://github.com/metallb/metallb/tree/main/charts/metallb
