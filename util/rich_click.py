bf7eb8847462d2a2bc794e71984fdeefd1edeed6
Jesse Hitch <jessebot@linux.com>
2022-09-30 10:45:13 +0200

    update to rich-er displays, fixing colors, changing lib directory to 'util'

--- util/rich_click.py
+++ util/rich_click.py
@@ -24,20 +24,23 @@ class RichCommand(click.Command):
 
         highlighter = OptionHighlighter()
 
-        console = Console(theme=Theme({"option": "bold cyan",
-                                       "switch": "bold green"}),
+        console = Console(theme=Theme({"option": "bold light_steel_blue",
+                                       "switch": "bold royal_blue1"}),
                           highlighter=highlighter)
 
-        header = ("[b]Smol K8s Lab[/b]\n\n[dim]Quickly install a k8s distro "
-                  "for a lab setup. Installs k3s with metallb, "
-                  "nginx-ingess-controller, cert-manager, and argocd")
+        title = "☁ [cornflower_blue][b]Smol K8s Lab[/]\n"
+        desc = ("[dim]Quickly install a k8s distro for a lab setup. Installs "
+                "k3s with metallb, nginx-ingess-controller, cert-manager, and "
+                "argocd")
 
-        console.print(header, justify="center")
+        console.print(title + desc, justify="center")
 
-        console.print("\nUsage: [b]smol-k8s-lab.py[/b] [b cyan]<k3s,kind> " +
-                      "[b][OPTIONS][/] \n")
+        console.print("\n[b]Usage[/b]: smol-k8s-lab.py [cornflower_blue]" +
+                      "<k3s OR kind> [royal_blue1][OPTIONS]\n")
 
-        options_table = Table(highlight=True, box=None, show_header=False)
+        options_table = Table(highlight=True, box=None, show_header=False,
+                              row_styles=["bold", ""],
+                              padding=(1, 1, 0, 0))
 
         for param in self.get_params(ctx)[1:]:
 
@@ -49,7 +52,8 @@ class RichCommand(click.Command):
                 opt1 = Text("")
 
             if param.metavar:
-                opt2 += Text(f" {param.metavar}", style="bold yellow")
+                opt2 += Text(f" {param.metavar}",
+                             style="bold light_steel_blue")
 
             options = Text(" ".join(reversed(param.opts)))
             help_record = param.get_help_record(ctx)
@@ -64,5 +68,6 @@ class RichCommand(click.Command):
 
             options_table.add_row(opt1, opt2, highlighter(help))
 
-        console.print(Panel(options_table, border_style="dim", title="Options",
+        console.print(Panel(options_table, border_style="dim light_steel_blue",
+                            title="ʕ ᵔᴥᵔʔ Options",
                             title_align="left"))
