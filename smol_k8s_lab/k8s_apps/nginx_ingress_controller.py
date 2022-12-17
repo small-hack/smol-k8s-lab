47b40f57a8706af223b84ea43e1101eb7b3987bd
Jesse Hitch <jessebot@linux.com>
2022-12-17 18:33:47 +0100

    fix url declaration to only happy if kind is distro

--- smol_k8s_lab/k8s_apps/nginx_ingress_controller.py
+++ smol_k8s_lab/k8s_apps/nginx_ingress_controller.py
@@ -16,10 +16,11 @@ def configure_ingress_nginx(k8s_distro="k3s"):
     # nginx_chart_opts = {'hostNetwork': 'true','hostPort.enabled': 'true'}
     # set_options=nginx_chart_opts)
     """
-    url = ('https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/'
-           'deploy/static/provider/kind/deploy.yaml')
-
     if k8s_distro == 'kind':
+        # TODO: Pin this version?
+        url = ('https://raw.githubusercontent.com/kubernetes/ingress-nginx'
+               '/main/deploy/static/provider/kind/deploy.yaml')
+
         # this is to wait for the deployment to come up
         apply_manifests(url, "ingress-nginx", "ingress-nginx-controller",
                         "app.kubernetes.io/component=controller")
