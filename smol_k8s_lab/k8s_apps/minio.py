437958f672c75ecedd4e1c46515bda105075a044
Jesse Hitch <jessebot@linux.com>
2023-01-04 11:51:25 +0000

    add a better 'toy' setup of minio

--- smol_k8s_lab/k8s_apps/minio.py
+++ smol_k8s_lab/k8s_apps/minio.py
@@ -5,9 +5,12 @@ DESCRIPTION: install minio, a local S3 compatible object store
      AUTHOR: @jessebot
     LICENSE: GNU AFFERO GENERAL PUBLIC LICENSE Version 3
 """
+from os import path
+from yaml import dump
 from ..k8s_tools.homelabHelm import helm
 from ..bw_cli import BwCLI
 from ..console_logging import header, sub_header, print_msg
+from ..env_config import XDG_CACHE_DIR
 
 
 def install_minio(password_manager=False):
@@ -20,9 +23,38 @@ def install_minio(password_manager=False):
     # this is to generate a secure password
     minio_password = bw.generate()
 
+    CIDR = "0.0.0.0/0"
     # for set options in helm
-    val = {'rootUser': 'minio_admin', 'rootPassword': minio_password,
-           'replicas': 2}
+    val = {'rootUser': 'minio_admin',
+           'rootPassword': minio_password,
+           'resources': {'requests': {'memory': '1Gi'}},
+           'replicas': 1,
+           'mode': 'standalone',
+           'service': {
+               'type': 'LoadBalancer'
+               },
+           'consoleService': {
+               'type': 'LoadBalancer'
+               },
+           'ingress': {
+               'enabled': 'true',
+               'ingressClassName': "nginx",
+               'hosts': ["minio.vleermuis.tech"],
+               'annotations': {
+                   "kubernetes.io/ingress.allow-http": 'true',
+                   "nginx.ingress.kubernetes.io/whitelist-source-range": CIDR,
+                   }
+               },
+           'consoleIngress': {
+               'enabled': 'true',
+               'ingressClassName': "nginx",
+               'hosts': ["console.minio.vleermuis.tech"],
+               'annotations': {
+                   "kubernetes.io/ingress.allow-http": 'true',
+                   "nginx.ingress.kubernetes.io/whitelist-source-range": CIDR,
+                   }
+               }
+           }
 
     # if we're using a password manager, generate a password & save it
     if password_manager:
@@ -38,9 +70,14 @@ def install_minio(password_manager=False):
         sub_header("Here are MinIO root credentials. [i]Please store them!")
         print_msg(f"rootUser: minio_admin\nrootPassword: {minio_password}")
 
+    # this creates a values.yaml from from the val dict above
+    values_file_name = path.join(XDG_CACHE_DIR, 'minio_values.yaml')
+    with open(values_file_name, 'w') as values_file:
+        dump(val, values_file)
+
     release = helm.chart(release_name='minio',
                          chart_name='minio/minio',
                          namespace='minio',
-                         set_options=val)
+                         values_file=values_file_name)
     release.install()
     return True
