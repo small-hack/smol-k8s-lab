e5bf0a932246c8b901795eabce89286c80628d26
jessebot <jessebot@linux.com>
2023-08-03 11:18:44 +0200

    upgrading certmanager version to 1.12.3

--- smol_k8s_lab/k8s_apps/certmanager.py
+++ smol_k8s_lab/k8s_apps/certmanager.py
@@ -11,29 +11,33 @@ from ..k8s_tools.kubernetes_util import apply_custom_resources
 
 def configure_cert_manager(email_addr):
     """
-    installs cert-manager helm chart and letsencrypt-staging clusterissuer
+    installs cert-manager helm chart and letsencrypt clusterissuers
     """
 
     # install chart and wait
     release = helm.chart(release_name='cert-manager',
                          chart_name='jetstack/cert-manager',
-                         chart_version="1.12.0",
+                         chart_version="1.12.3",
                          namespace='kube-system',
                          set_options={'installCRDs': 'true'})
     release.install(True)
 
+    # we create a ClusterIssuer for both staging and prod
+    issuers = []
     acme_staging = 'https://acme-staging-v02.api.letsencrypt.org/directory'
-    issuer = {'apiVersion': 'cert-manager.io/v1',
-              'kind': 'ClusterIssuer',
-              'metadata': {'name': 'letsencrypt-staging'},
-              'spec': {
-                  'acme': {'email': email_addr,
-                           'server': acme_staging,
-                           'privateKeySecretRef': {
-                               'name': 'letsencrypt-staging'},
-                           'solvers': [
-                               {'http01': {'ingress': {'class': 'nginx'}}}]
-                           }}}
+    for issuer in ['letsencrypt-prod', 'letsencrypt-staging']:
+        issuers.append({
+            'apiVersion': 'cert-manager.io/v1',
+            'kind': 'ClusterIssuer',
+            'metadata': {'name': issuer},
+            'spec': {'acme': {'email': email_addr,
+                              'server': acme_staging,
+                              'privateKeySecretRef': {
+                                  'name': 'letsencrypt-staging'
+                                  },
+                              'solvers': [{'http01': {'ingress':
+                                                      {'class': 'nginx'}}}]
+                               }}})
 
-    apply_custom_resources([issuer])
+    apply_custom_resources(issuers)
     return True
