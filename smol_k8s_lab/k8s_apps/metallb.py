f744e4342f82acf1b82b78c8420b072af0b4c67c
jessebot <jessebot@linux.com>
2023-08-17 21:32:26 +0200

    add metallb fix

--- smol_k8s_lab/k8s_apps/metallb.py
+++ smol_k8s_lab/k8s_apps/metallb.py
@@ -9,12 +9,12 @@ import logging as log
 from ..k8s_tools.kubernetes_util import apply_custom_resources, apply_manifests
 
 
-def configure_metallb(address_pool=[]):
+def configure_metallb(address_pool: str = ""):
     """
     metallb is special because it has Custom Resources:
         IPaddressPool and L2Advertisement
     Requires and accepts one arg:
-        address_pool - list of IP addresses - default: []
+        address_pool - comma seperated list of IP addresses
     """
     url = ("https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/"
            "manifests/metallb-native.yaml")
@@ -30,7 +30,7 @@ def configure_metallb(address_pool=[]):
                   'kind': 'IPAddressPool',
                   'metadata': {'name': 'default',
                                'namespace': 'metallb-system'},
-                  'spec': {'addresses': address_pool}}
+                  'spec': {'addresses': address_pool.split(',')}}
 
     l2_advert_cr = {'apiVersion': 'metallb.io/v1beta1',
                     'kind': 'L2Advertisement',
