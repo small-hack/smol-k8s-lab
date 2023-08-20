e1a7eddd1c3ffb6d9019ea5617ea859bcf3ef605
jessebot <jessebot@linux.com>
2023-08-20 13:31:01 +0200

    add getting data parameter the correct way for the zitadel secret

--- smol_k8s_lab/k8s_apps/zitadel.py
+++ smol_k8s_lab/k8s_apps/zitadel.py
@@ -115,7 +115,7 @@ def configure_zitadel(k8s_obj: K8s,
     sub_header("Configure zitadel as your OIDC SSO for Argo CD")
 
     # setup the zitadel python api wrapper
-    adm_secret = k8s_obj.get_secret('zitadel-admin-sa', 'zitadel')['data']
+    adm_secret = k8s_obj.get_secret('zitadel-admin-sa', 'zitadel').data
     adm_secret_file = adm_secret['zitadel-admin-sa.json']
     api_token = b64dec(bytes(adm_secret_file))
     zitadel =  Zitadel(f"https://{zitadel_hostname}/management/v1/", api_token)
