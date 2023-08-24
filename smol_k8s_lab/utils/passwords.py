ef1f4ff8fa40754779856cacaeb5835ab0df2e0c
jessebot <jessebot@linux.com>
2023-08-24 12:08:12 +0200

    allow a special character in the create password function

--- smol_k8s_lab/utils/passwords.py
+++ smol_k8s_lab/utils/passwords.py
@@ -1,16 +1,18 @@
 import secrets
 import string
+from random import randrange
 
 
-def create_password():
+def create_password(special_character: bool = False) -> str:
     """
     Generate a 32 alphanumeric password with at least one lowercase character,
     at least one uppercase character, and at least three digits:
     https://docs.python.org/3/library/secrets.html#recipes-and-best-practices
 
-    Takes no arguments.
+    Takes optional special_character bool. If true, sets a random character in
+    the password string to . (period).
 
-    returns password.
+    returns password str
     """
     alphabet = string.ascii_letters + string.digits
     while True:
@@ -19,4 +21,12 @@ def create_password():
                 and any(c.isupper() for c in password)
                 and sum(c.isdigit() for c in password) >= 3):
             break
+
+    # if special character requested in the password, we'll replace a random
+    # random character in the password string with periods
+    if special_character:
+        random_number = randrange(0,31)
+        replace_character = password[random_number]
+        password = password.replace(replace_character, '.')
+
     return password
