59fd8fa162561b2a9ae2b750fd17fdc6a31042dd
jessebot <jessebot@linux.com>
2023-08-25 16:48:56 +0200

    fix zitadel vouch app creation

--- smol_k8s_lab/k8s_apps/oidc.py
+++ smol_k8s_lab/k8s_apps/oidc.py
@@ -19,21 +19,35 @@ def setup_oidc_provider(k8s_obj: K8s,
     if provider_name == 'keycloak':
         log.debug("Setting up keycloak")
         configure_keycloak(k8s_obj, provider_dict, bw)
-        extra = {'realm': provider_dict['argo']['secret_keys']['default_realm']}
+        realm = provider_dict['argo']['secret_keys']['default_realm']
 
     # setup zitadel if we're using that for OIDC
     elif provider_name == 'zitadel':
         log.debug("Setting up zitadel")
-        extra = {'zitadel': configure_zitadel(k8s_obj, provider_dict, argocd_fqdn, bw)}
+        zitadel = configure_zitadel(k8s_obj, provider_dict, argocd_fqdn, bw)
 
     if vouch_dict:
         if vouch_dict['enabled']:
             log.debug("Setting up vouch")
-            configure_vouch(k8s_obj,
-                            vouch_dict,
-                            provider_name,
-                            provider_dict['argo']['secret_keys']['hostname'],
-                            bw,
-                            **extra)
-
+            if provider_name == 'keycloak':
+                configure_vouch(k8s_obj,
+                                vouch_dict,
+                                provider_name,
+                                provider_dict['argo']['secret_keys']['hostname'],
+                                bw,
+                                realm)
+            elif provider_name == 'zitadel':
+                configure_vouch(k8s_obj,
+                                vouch_dict,
+                                provider_name,
+                                provider_dict['argo']['secret_keys']['hostname'],
+                                bw,
+                                None,
+                                zitadel)
+            else:
+                configure_vouch(k8s_obj,
+                                vouch_dict,
+                                provider_name,
+                                provider_dict['argo']['secret_keys']['hostname'],
+                                bw)
     return True
