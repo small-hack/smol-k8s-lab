55a717eec4e1b804893acf7c74c042c101dfb1c5
jessebot <jessebot@linux.com>
2023-09-08 11:10:29 +0200

    install cilium to match argocd applicationset values

--- smol_k8s_lab/k8s_apps/networking/cilium.py
+++ smol_k8s_lab/k8s_apps/networking/cilium.py
@@ -7,21 +7,45 @@ DESCRIPTION: configures cilium
 """
 import logging as log
 from smol_k8s_lab.k8s_tools.helm import Helm
+from smol_k8s_lab.constants import XDG_CACHE_DIR
+from os import path
+import yaml
 
 
-def configure_cilium():
+def configure_cilium(cilium_dict: dict):
     """
     installs cilium from the manifests in their official repo
-
     """
     log.info("Installing Cilium now")
+
+    cilium_hostname = cilium_dict['argo']['secret_keys']['hostname']
+
+    values = {"operator": {"replicas": 1},
+              "hubble": {"relay": {"enabled": True},
+                         "ui": {"enabled": True,
+                                "ingress": {"enabled": True,
+                                            "className": "nginx",
+                                            "hosts": [cilium_hostname],
+                                            "tls": [
+                                                {"secretName": "cilium-tls",
+                                                 "hosts": [cilium_hostname]}
+                                                ]
+                                            }
+                                }
+                         }
+              }
+
+    # this creates a values.yaml from the val dict above
+    values_file_name = path.join(XDG_CACHE_DIR, 'cilium_values.yaml')
+    with open(values_file_name, 'w') as values_file:
+        yaml.dump(values, values_file)
 
     # install chart and wait
     release = Helm.chart(release_name='cilium',
                          chart_name='cilium/cilium',
                          chart_version="1.14.1",
                          namespace='cilium',
-                         set_options={'operator.replicas': 1})
+                         values_file=values_file_name)
     release.install(True)
 
     return True
