5ab74910a23fea625db7fc5b4de062fe3bfb7934
jessebot <jessebot@linux.com>
2023-09-10 14:17:42 +0200

    adding more styling

--- smol_k8s_lab/tui/select_apps.py
+++ smol_k8s_lab/tui/select_apps.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.11
 from textual import on
 from textual.app import App, ComposeResult
+from textual.binding import Binding
 from textual.containers import Horizontal
 from textual.events import Mount
 from textual.widgets import Footer, Header, Pretty, SelectionList
@@ -13,38 +14,44 @@ DEFAULT_APPS = DEFAULT_CONFIG['apps']
 
 class InitialApps(App[None]):
     CSS_PATH = "select_apps.tcss"
+    BINDINGS = [
+        Binding(key="j", action="down", description="Scroll down"),
+        Binding(key="k", action="up", description="Scroll up"),
+        Binding(key="q", action="quit", description="Quit the app")
+    ]
 
     def compose(self) -> ComposeResult:
         """
         initialize app contents
         """
-        yield Header()
+        header = Header()
+        header.tall = True
+        yield header
         with Horizontal():
             full_list = []
             for argocd_app, app_metadata in DEFAULT_APPS.items():
                 if argocd_app == 'cilium':
-                    full_list.append(
-                            Selection("cilium - an app for ebpf stuff",
-                                      "cilium",
-                                      False)
-                                     )
+                    full_list.append(Selection("cilium - an app for ebpf stuff",
+                                               "cilium",
+                                               False))
                 elif argocd_app != 'cilium' and app_metadata['enabled']:
-                    full_list.append(
-                            Selection(argocd_app.replace("_","-"), argocd_app, True)
-                            )
+                    full_list.append(Selection(argocd_app.replace("_","-"),
+                                               argocd_app,
+                                               True))
                 else:
-                    full_list.append(
-                            Selection(argocd_app.replace("_","-"), argocd_app)
-                            )
+                    full_list.append(Selection(argocd_app.replace("_","-"),
+                                               argocd_app))
 
             yield SelectionList[str](*full_list)
             yield Pretty([])
         yield Footer()
 
     def on_mount(self) -> None:
+        self.screen.styles.border = ("heavy", "cornflowerblue")
         self.title = "🧸smol k8s lab"
         self.sub_title = "now with more 🦑"
-        self.query_one(SelectionList).border_title = "Shall we install some k8s apps?"
+        cute_question = "ʕ ᵔᴥᵔʔ Select which apps to install on k8s"
+        self.query_one(SelectionList).border_title = cute_question
         self.query_one(Pretty).border_title = "Selected Apps"
 
     @on(Mount)
