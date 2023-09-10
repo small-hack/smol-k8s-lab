89e8adb93e42f88be7bb2423f24c6c5b1b08b912
jessebot <jessebot@linux.com>
2023-09-10 20:17:50 +0200

    add rest of currently created TUI to to launch_tui function

--- smol_k8s_lab/utils/tui/__init__.py
+++ smol_k8s_lab/utils/tui/__init__.py
@@ -1,9 +1,19 @@
 from .select_distro import SelectDistro
+from .select_apps import SelectApps
+from .configure_apps import ConfigureApps
+import logging as log
 
 
 def launch_tui():
     """
     Run all the TUI screens
     """
-    SelectDistro().run()
+    selected_distros = SelectDistro().run()
+    log.info(f"selected_distros: {selected_distros}")
+
+    selected_apps = SelectApps().run()
+    log.info(f"selected_apps: {selected_apps}")
+
+    ConfigureApps().run()
+
     return True
