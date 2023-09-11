e6359837927f16693de2ec827ec53b6382c28875
jessebot <jessebot@linux.com>
2023-09-11 19:50:44 +0200

    clean up testing files and slim down tui to just one file for now

--- smol_k8s_lab/utils/tui/__init__.py
+++ smol_k8s_lab/utils/tui/__init__.py
@@ -1,20 +1,10 @@
-from .select_distro import SelectK8sDistro
-from .select_apps import SelectApps
-from .configure_apps import ConfigureApps
-import logging as log
+from .configure_all import ConfigureAll
 
 
 def launch_tui():
     """
     Run all the TUI screens
     """
-    distros = SelectK8sDistro().run()
-    log.info(f"selected_distro: {distros}")
-
-    selected_apps = SelectApps().run()
-    log.info(f"selected_apps: {selected_apps}")
-
-    configured_apps = ConfigureApps(selected_apps).run()
-    log.info(configured_apps)
+    ConfigureAll().run()
 
     return True
