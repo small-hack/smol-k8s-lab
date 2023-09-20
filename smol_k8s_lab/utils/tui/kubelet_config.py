f6452a0264f1eff99e738a510ff3a9debc3955f9
jessebot <jessebot@linux.com>
2023-09-20 17:29:45 +0200

    more updates to the help screen and kubelet css

--- smol_k8s_lab/utils/tui/kubelet_config.py
+++ smol_k8s_lab/utils/tui/kubelet_config.py
@@ -2,6 +2,16 @@
 from textual.app import ComposeResult
 from textual.containers import VerticalScroll, Horizontal
 from textual.widgets import Input, Button, Static
+from textual.suggester import SuggestFromList
+
+SUGGESTIONS = SuggestFromList((
+        "podsPerCore",
+        "maxPods",
+        "node-labels",
+        "featureGates"
+        ))
+
+VALUE_SUGGESTIONS = SuggestFromList(("ingress-ready=true"))
 
 
 class KubeletConfig(Static):
@@ -18,25 +28,13 @@ class KubeletConfig(Static):
         # kubelet config section
         with VerticalScroll(classes=f"kubelet-config-scroll {self.distro}",
                             id=f"{self.distro}-kubelet-config-container"):
-            # take extra kubelet config args
-            row_class = f"{self.distro} kubelet-arg"
-            row_container = Horizontal(classes=f"{row_class}-input-row")
 
             if self.kubelet_extra_args:
                 for key, value in self.kubelet_extra_args.items():
-                    with row_container:
-                        yield Input(value=key,
-                                    placeholder="optional kubelet config key arg",
-                                    classes=f"{row_class}-input-key")
+                    yield self.generate_row(key, str(value))
 
-                        yield Input(value=str(value),
-                                    placeholder=key,
-                                    classes=f"{row_class}-input-value")
-
-                        yield Button("🚮",
-                                     classes=f"{row_class}-del-button")
-
-            yield Button("➕ Add New Arg", classes=f"{row_class}-add-button")
+            yield Button("➕ Add New Arg",
+                         classes=f"{self.distro} kubelet-arg-add-button")
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """
@@ -45,20 +43,41 @@ class KubeletConfig(Static):
         button_classes = event.button.classes
 
         # lets you delete a kubelet-arg row
-        if "kubelet-arg-del-button" in button_classes:
+        if "kubelet-arg-input-del-button" in button_classes:
             parent_row = event.button.parent
             parent_row.remove()
 
+        # add a new row of kubelet arg inputs before the add button
         if "kubelet-arg-add-button" in button_classes:
             parent_container = event.button.parent
-            row_class = f"{self.distro} kubelet-arg"
+            parent_container.mount(self.generate_row(), before=event.button)
 
-            # add a new row of kubelet arg inputs before the add button
-            parent_container.mount(Horizontal(
-                Input(placeholder="optional kubelet config key arg",
-                      classes=f"{row_class}-input-key"),
-                Input(placeholder="optional kubelet config key arg",
-                      classes=f"{row_class}-input-value"),
-                Button("🚮", classes=f"{row_class}-del-button"),
-                       classes=f"{row_class}-input-row"),
-                before=event.button)
+    def generate_row(self, param: str = "", value: str = "") -> None:
+        """ 
+        generate a new input field set
+        """
+        # base class for all the below object
+        row_class = f"{self.distro} kubelet-arg-input"
+
+        # first input field
+        param_input_args = {"placeholder": "optional kubelet parameter",
+                            "classes": f"{row_class}-key",
+                            "suggester": SUGGESTIONS}
+        if param:
+            param_input_args["value"] = param
+        param_input = Input(**param_input_args)
+
+        # second input field
+        param_value_input_args = {"classes": f"{row_class}-value",
+                                  "placeholder": "kubelet parameter value",
+                                  "suggester": VALUE_SUGGESTIONS}
+        if value:
+            param_value_input_args["value"] = value
+        param_value_input = Input(**param_value_input_args)
+
+        # delete button for each row
+        del_button = Button("🚮", classes=f"{row_class}-del-button")
+        del_button.tooltip = "Delete this kubelet parameter"
+
+        return Horizontal(param_input, param_value_input, del_button,
+                          classes=f"{row_class}-row")
