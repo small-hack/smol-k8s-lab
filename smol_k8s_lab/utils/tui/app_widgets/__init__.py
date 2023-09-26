bffb4eb00435c90377b65b9115e21fbd14854ae1
jessebot <jessebot@linux.com>
2023-09-26 10:56:22 +0200

    separating out more widgets for app inputs

---
+++ smol_k8s_lab/utils/tui/app_widgets/__init__.py
@@ -0,0 +1,20 @@
+def placeholder_grammar(key: str):
+    article = ""
+
+    # make sure this isn't a plural key
+    plural = key.endswith('s')
+
+    if key == "address_pool":
+        plural = True
+
+    # create a gramatically corrrect placeholder
+    if key.startswith(('o','a','e')) and not plural:
+        article = "an"
+    elif not plural:
+        article = "a"
+
+    # if this is plural
+    if plural:
+        return f"enter comma seperated list of {key}"
+    else:
+        return f"enter {article} {key}"
