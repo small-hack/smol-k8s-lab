d10b2110dc197a4a7ae7049b8263fb6fc99076df
jessebot <jessebot@linux.com>
2023-09-27 14:57:57 +0200

    mostly got the right hand side panel working and but vertical scroll still not taking effect and not sure why

--- smol_k8s_lab/utils/tui/app_widgets/app_inputs_confg.py
+++ smol_k8s_lab/utils/tui/app_widgets/app_inputs_confg.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3.11
-from smol_k8s_lab.utils.tui.app_widgets.init_values_widget import InitValuesWidget
-from smol_k8s_lab.utils.tui.app_widgets.argocd_widgets import ArgoCDAppProjInputs
+from . import placeholder_grammar
+from textual import on
 from textual.app import ComposeResult
-from textual.containers import Horizontal, VerticalScroll
-from textual.widget import Widget
-from textual.widgets import Input, Button
+from textual.containers import Container, Horizontal, VerticalScroll
+from textual.validation import Length
+from textual.widgets import Input, Label, Button, Switch, Static
 from textual.widgets.selection_list import Selection
 
 
-class AddAppInput(Widget):
+ARGO_TOOLTIPS = {'repo': 'URL to a git repository where you have k8s manifests ' + \
+                         '(including Argo resources) to deploy',
+                 'path': 'path in repo to resources you want to deploy',
+                 'ref': 'branch or tag to point to in the repo',
+                 'namespace': 'k8s namespace to deploy the Argo CD App in'}
+
+
+class AddAppInput(Static):
     """
     Add new inputs for new application
     """
@@ -56,18 +63,6 @@ class AddAppInput(Widget):
         # add this app to the possible invalid inputs
         parent_app.invalid_app_inputs[underscore_app_name] = []
 
-        # creates a new hidden app inputs view for the new application
-        inputs = VerticalScroll(
-                AppInputs(
-                    underscore_app_name,
-                    parent_app.cfg['apps'][underscore_app_name]
-                    ),
-                id=f"{underscore_app_name}-inputs"
-                )
-
-        inputs.display = False
-        parent_app.app.get_widget_by_id("app-inputs-pane").mount(inputs)
-
         # adds selection to the app selection list
         selection_list = parent_app.get_widget_by_id("selection-list-of-apps")
         selection_list.add_option(Selection(underscore_app_name.replace("_", "-"),
@@ -76,16 +71,314 @@ class AddAppInput(Widget):
         input.value = ""
 
 
-class AppInputs(Widget):
+class AppInputs(Static):
     """
     Display inputs for given smol-k8s-lab supported application
     """
-    def __init__(self, app_name: str, metadata: dict) -> None:
+    def __init__(self, app_name: str, config_dict: dict) -> None:
         self.app_name = app_name
-        self.metadata = metadata
+        self.argo_params = config_dict['argo']
+        self.init = config_dict.get('init', None)
         super().__init__()
 
     def compose(self) -> ComposeResult:
-        yield InitValuesWidget(self.app_name, self.metadata.get('init', None))
-        with VerticalScroll(id=f"{self.app_name}-inputs", classes="single-app-inputs"):
-            yield ArgoCDAppProjInputs(self.app_name, self.metadata['argo'])
+        with Container(classes="init-widget"):
+            yield InitValues(self.app_name, self.init)
+
+        # standard values to source an argo cd app from an external repo
+        with Container(classes=f"{self.app_name} argo-config-container"):
+            yield Label("Advanced Argo CD App Configuration",
+                        classes=f"{self.app_name} argo-config-header")
+
+            for key, value in ARGO_TOOLTIPS.items():
+                input = Input(placeholder=f"Please enter a {key}",
+                              value=self.argo_params[key],
+                              name=key,
+                              validators=[Length(minimum=2)],
+                              id=f"{self.app_name}-{key}",
+                              classes=f"{self.app_name} argo-config-input")
+                input.tooltip = value
+
+                if input.validate(self.argo_params[key]):
+                    invalid_inputs = self.ancestors[-1].invalid_app_inputs
+                    app_inputs = invalid_inputs.get(self.app_name, None)
+                    if app_inputs:
+                        app_inputs.append(key)
+                    else:
+                        invalid_inputs[self.app_name] = [key]
+
+                yield Horizontal(
+                        Label(f"{key}:",
+                              classes=f"{self.app_name} argo-config-label"),
+                              input,
+                              classes=f"{self.app_name} argo-config-row")
+
+            # secret keys
+            label =  Label("Template values to pass to Argo CD ApplicationSet ",
+                           classes="secret-key-divider")
+            label.tooltip = ("🔒Added to k8s secret for the Argo CD "
+                             "ApplicationSet Secret Plugin Generator")
+            yield label
+
+            secret_keys = self.argo_params.get('secret_keys', None)
+
+            if secret_keys:
+                # iterate through the app's secret keys
+                for secret_key, value in secret_keys.items():
+                    yield self.generate_secret_key_row(secret_key, value)
+            else:
+                help = ("smol-k8s-lab doesn't include any templated values for"
+                        " this app, but you can add some below if you're using "
+                        "a custom Argo CD App repo.")
+                yield Label(help, classes="secret-key-help-text")
+
+            key_input = Input(placeholder="new key name",
+                              id=f"{self.app_name}-new-secret",
+                              classes="new-secret-input")
+
+            key_input.tooltip = ("🔒key name to pass to the Argo CD ApplicationSet"
+                                 " Secret Plugin Generator for templating non-"
+                                 "sensitive values such as hostnames.")
+
+            yield Horizontal(Button("➕",
+                                    id=f"{self.app_name}-new-secret-button",
+                                    classes="new-secret-button"),
+                             key_input,
+                             classes="app-input-row")
+
+            # argocd project configuration
+            yield Label("Advanced Argo CD App Project Configuration",
+                        classes=f"{self.app_name} argo-config-header")
+
+            # row for project destination namespaces
+            with Horizontal(classes=f"{self.app_name} argo-config-row"):
+                label = Label("namespaces:",
+                              classes=f"{self.app_name} argo-config-label")
+                label.tooltip = "Comma seperated list of namespaces"
+                yield label
+
+                n_spaces = self.argo_params["project"]["destination"]["namespaces"]
+                classes = f"{self.app_name} argo-config-input argo-proj-ns"
+                yield Input(placeholder="Enter comma seperated list of namespaces",
+                            name="namespaces",
+                            value=", ".join(n_spaces),
+                            classes=classes)
+
+            # row for project source repos
+            with Horizontal(classes=f"{self.app_name} argo-config-row"):
+                label = Label("source_repos:",
+                              classes=f"{self.app_name} argo-config-label")
+                label.tooltip = "Comma seperated list of project source repos"
+                yield label
+
+                repos = self.argo_params["project"]["source_repos"]
+                classes = f"{self.app_name} argo-config-input argo-proj-repo"
+                yield Input(placeholder="Please enter source_repos",
+                            value=", ".join(repos),
+                            name="source_repos",
+                            classes=classes)
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """
+        add a new input row for secret stuff
+        """
+        if event.button.id == f"{self.app_name}-new-secret-button":
+            inputs_box = self.get_widget_by_id(f"{self.app_name}-init-inputs")
+            input = self.get_widget_by_id(f"{self.app_name}-new-secret").value
+
+            if input:
+                # root app yaml
+                apps_yaml = self.app.ancestors[-1].cfg['apps'][self.app_name]
+                apps_yaml['argo']['secret_keys'][input] = ""
+
+                # add new secret key row
+                inputs_box.mount(self.generate_secret_key_row(input), before=0)
+
+    def generate_secret_key_row(self, secret_key: str, value: str = "") -> None:
+        """
+        add a new row of secret keys to pass to the argocd app
+        """
+        key_label = secret_key.replace("_", " ")
+
+        # create input
+        input_class = f"app-secret-key-input {self.app_name}"
+        input_keys = {"placeholder": placeholder_grammar(key_label),
+                      "classes": input_class,
+                      "name": secret_key,
+                      "validators": [Length(minimum=2)]}
+        if value:
+            input_keys['value'] = value
+
+        input = Input(**input_keys)
+
+        if input.validate(value):
+            invalid_inputs = self.ancestors[-1].invalid_app_inputs
+            app_inputs = invalid_inputs.get(self.app_name, None)
+
+            if app_inputs:
+                app_inputs.append(secret_key)
+            else:
+                invalid_inputs[self.app_name] = [secret_key]
+
+        # create the input row
+        secret_label = Label(f"{key_label}:",
+                             classes=f"app-input-label {self.app_name}")
+        return Horizontal(secret_label, input,
+                          classes=f"app-input-row {self.app_name}")
+
+    @on(Input.Changed)
+    def update_base_yaml(self, event: Input.Changed) -> None:
+        input = event.input
+        parent_app = input.ancestors[-1]
+        parent_app_yaml = parent_app.cfg['apps'][self.app_name]
+
+        # if this is an Argo CD app/project config input
+        if "argo-config-input" in input.classes:
+
+            # if this is a project source repo
+            if "argo-proj-repo" in input.classes:
+                values = input.value.replace(" ","").split(",")
+                parent_app_yaml['argo']['project'][input.name] = values
+
+            # if this is a project destination namespace
+            elif "argo-proj-ns" in input.classes:
+                values = input.value.replace(" ","").split(",")
+                parent_app_yaml['argo']['project']['destination'][input.name] = values
+
+            # otherwise it's probably just a normal app input
+            else:
+                parent_app_yaml['argo'][input.name] = input.value
+
+        # if this is a secret key input
+        elif "app-secret-key-input" in input.classes:
+            parent_app_yaml['argo']['secret_keys'][input.name] = input.value
+
+        # Updating the main app with k8s app that has validation failed
+        if event.validation_result:
+            invalid_inputs = parent_app.invalid_app_inputs
+            if not event.validation_result.is_valid:
+                invalid_inputs[self.app_name].append(input.name)
+            else:
+                if input.name in invalid_inputs[self.app_name]:
+                    invalid_inputs[self.app_name].remove(input.name)
+
+        self.app.write_yaml()
+
+
+class InitValues(Static):
+    CSS_PATH = "../css/apps_init_config.tcss"
+    def __init__(self, app_name: str, init_dict: dict) -> None:
+        self.app_name = app_name
+        self.init = init_dict
+        if self.init:
+            self.init_values = self.init.get('values', None)
+        else:
+            self.init_values = None
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        # container for all inputs associated with one app
+        app_inputs_class = f"{self.app_name} init-inputs-container"
+        inputs_container = Container(id=f"{self.app_name}-init-inputs",
+                                     classes=app_inputs_class)
+
+        # make a pretty title with an init switch for the app to configure
+        s_class = f"app-init-switch-and-labels-row {self.app_name}"
+        with Container(classes=s_class):
+            init_lbl = Label("Initialization", classes="initialization-label")
+            init_lbl.tooltip = ("if supported, smol-k8s-lab will perform a "
+                                "one-time initial setup of this app")
+            yield init_lbl
+
+            yield Label("Enabled: ", classes="app-init-switch-label")
+
+            if not self.init:
+                switch = Switch(value=False,
+                                id=f"{self.app_name}-init-switch",
+                                classes="app-init-switch")
+
+                switch.tooltip = (f"Initialization for {self.app_name} is [i]not[/]"
+                                  " supported by smol-k8s-lab at this time")
+                switch.disabled = True
+                switch.animate = False
+            else:
+                init_enabled = self.init.get('enabled', False)
+
+                switch = Switch(value=init_enabled,
+                                id=f"{self.app_name}-init-switch",
+                                classes="app-init-switch")
+
+                if self.init_values and not init_enabled:
+                    inputs_container.display = False
+
+            yield switch
+
+        if self.init_values:
+            # these are special values that are only set up via
+            # smol-k8s-lab and do not live in a secret on the k8s cluster
+            with inputs_container:
+                for init_key, init_value in self.init_values.items():
+                    yield self.generate_init_row(init_key, init_value)
+
+    def generate_init_row(self, init_key: str, init_value: str) -> None:
+        # create input
+        input_keys = {"placeholder": placeholder_grammar(init_key),
+                      "classes": f"app-init-input {self.app_name}",
+                      "validators": [Length(minimum=2)],
+                      "name": init_key}
+        if init_value:
+            input_keys['value'] = init_value
+
+        # create the input row
+        label_class = f"app-input-label {self.app_name}"
+        label = Label(f"{init_key}: ", classes=label_class)
+        label.tooltip = (
+                "Init value for special one-time setup of this app."
+                " This value is [i]not[/i] stored in a secret for "
+                "later reference by Argo CD.")
+
+        container_class = f"app-input-row {self.app_name}"
+
+        input = Input(**input_keys)
+        if input.validate(init_value):
+            invalid_inputs = self.ancestors[-1].invalid_app_inputs
+            app_inputs = invalid_inputs.get(self.app_name, None)
+            if app_inputs:
+                app_inputs.append(init_key)
+            else:
+                invalid_inputs[self.app_name] = [init_key]
+
+        return Horizontal(label, input, classes=container_class)
+
+    @on(Switch.Changed)
+    def show_or_hide_init_inputs(self, event: Switch.Changed) -> None:
+        truthy_value = event.value
+
+        if self.init_values:
+            app_inputs = self.get_widget_by_id(f"{self.app_name}-init-inputs")
+            app_inputs.display = truthy_value
+
+        parent_app_yaml = event.switch.ancestors[-1].cfg['apps'][self.app_name]
+        parent_app_yaml['init']['enabled'] = truthy_value
+
+        event.switch.ancestors[-1].write_yaml()
+
+    @on(Input.Changed)
+    def update_base_yaml(self, event: Input.Changed) -> None:
+        input = event.input
+        parent_app = input.ancestors[-1]
+        parent_app_yaml = parent_app.cfg['apps'][self.app_name]
+        # if this is an init input
+        if "app-init-input" in input.classes:
+            parent_app_yaml['init']['values'][input.name] = input.value
+
+        # Updating the main app with k8s app that has validation failed
+        if event.validation_result:
+            invalid_inputs = parent_app.invalid_app_inputs
+            if not event.validation_result.is_valid:
+                invalid_inputs[self.app_name].append(input.name)
+            else:
+                if input.name in invalid_inputs[self.app_name]:
+                    invalid_inputs[self.app_name].remove(input.name)
+
+        self.app.write_yaml()
