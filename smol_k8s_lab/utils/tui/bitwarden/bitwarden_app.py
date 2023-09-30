6f29e18795f431d2f378fc546bd62d110124fa1e
jessebot <jessebot@linux.com>
2023-09-30 14:57:48 +0200

    throw some inline CSS at the bitwarden app because not a lot is going on

--- smol_k8s_lab/utils/tui/bitwarden/bitwarden_app.py
+++ smol_k8s_lab/utils/tui/bitwarden/bitwarden_app.py
@@ -6,8 +6,47 @@ from textual.widgets import Label
 
 class ReturnBitwardenObj(App[None]):
     CSS = """
+        $bluish_white: rgb(189,216,255);
+        $lavender: rgb(174,168,248);
+        $neon_magenta: rgb(242,137,249);
+        $orange: rgb(253,205,54);
+        $spacechalk_lime: rgb(168,253,87);
+        $light_cornflower: rgb(122,162,247);
+        $blue_gray: rgb(86,95,137);
+        $dark_gray: rgb(58,58,58);
+        $navy: rgb(35,35,54);
+
         Screen {
            align: center middle;
+        }
+        Tooltip {
+          background: $navy 50%;
+          color: $bluish_white 75%;
+          border: dashed gray 80%;
+          padding-bottom: 0;
+          padding-left: 1;
+          padding-right: 1;
+        }
+
+        Button {
+          color: $light_cornflower;
+          background: $lavender 10%;
+          border-top: tall $blue_gray 60%;
+          border-bottom: tall $dark_gray 50%;
+        }
+
+        Input {
+          background: $bluish_white 8%;
+          color: white 75%;
+          border: tall black 18%;
+        }
+
+        Input.-invalid {
+           border: tall $neon_magenta 90%;
+        }
+
+        Input.-valid:focus {
+           border: tall $spacechalk_lime 70%;
         }
         """
 
