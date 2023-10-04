c87cd310d898d7832f1b6c328172ee5fc5d81922
jessebot <jessebot@linux.com>
2023-10-04 19:09:27 +0200

    fix bitwarden bug from having moved to a different directory, and update the default config to use 'enabled' instead of always_enabled fr the smol_k8s_lab.interactive section

--- smol_k8s_lab/utils/bitwarden/bw_cli.py
+++ smol_k8s_lab/utils/bitwarden/bw_cli.py
@@ -19,7 +19,7 @@ from rich.prompt import Prompt
 from shutil import which
 from sys import exit
 from os import environ as env
-from .subproc import subproc
+from ..subproc import subproc
 from .bitwarden_dialog_app import AskUserForDuplicateStrategy
 
 
