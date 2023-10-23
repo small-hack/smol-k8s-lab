f2fc160b3ea79919d7aa933f78cc26a3438f1d86
jessebot <jessebot@linux.com>
2023-10-23 11:41:45 +0200

    finally finished the update of the social media secrets

--- smol_k8s_lab/k8s_apps/social/mastodon.py
+++ smol_k8s_lab/k8s_apps/social/mastodon.py
@@ -2,8 +2,11 @@ from rich.prompt import Prompt
 from smol_k8s_lab.bitwarden.bw_cli import BwCLI, create_custom_field
 from smol_k8s_lab.k8s_tools.argocd_util import install_with_argocd
 from smol_k8s_lab.k8s_tools.k8s_lib import K8s
+from smol_k8s_lab.k8s_tools.kubernetes_util import update_secret_key
 from smol_k8s_lab.utils.rich_cli.console_logging import sub_header, header
 from smol_k8s_lab.utils.passwords import create_password
+
+import logging as log
 
 
 def configure_mastodon(k8s_obj: K8s,
@@ -38,49 +41,87 @@ def configure_mastodon(k8s_obj: K8s,
             email_obj = create_custom_field("email", email)
             sub_header("Creating secrets in Bitwarden")
             password = bitwarden.generate()
-            bitwarden.create_login(name='mastodon-admin-credentials',
-                                   item_url=mastodon_hostname,
-                                   user=username,
-                                   password=password,
-                                   fields=[email_obj])
+            admin_id = bitwarden.create_login(
+                    name='mastodon-admin-credentials',
+                    item_url=mastodon_hostname,
+                    user=username,
+                    password=password,
+                    fields=[email_obj]
+                    )
 
             # PostgreSQL credentials
             mastodon_pgsql_password = bitwarden.generate()
             postrges_pass_obj = create_custom_field("postrgesPassword",
                                                     mastodon_pgsql_password)
-            bitwarden.create_login(name='mastodon-pgsql-credentials',
-                                   item_url=mastodon_hostname,
-                                   user='mastodon',
-                                   password=mastodon_pgsql_password,
-                                   fields=[postrges_pass_obj])
+            db_id = bitwarden.create_login(
+                    name='mastodon-pgsql-credentials',
+                    item_url=mastodon_hostname,
+                    user='mastodon',
+                    password=mastodon_pgsql_password,
+                    fields=[postrges_pass_obj]
+                    )
 
             # Redis credentials
             mastodon_redis_password = bitwarden.generate()
-            bitwarden.create_login(name='mastodon-redis-credentials',
-                                   item_url=mastodon_hostname,
-                                   user='mastodon',
-                                   password=mastodon_redis_password)
+            redis_id = bitwarden.create_login(
+                    name='mastodon-redis-credentials',
+                    item_url=mastodon_hostname,
+                    user='mastodon',
+                    password=mastodon_redis_password
+                    )
 
             # SMTP credentials
-            mastodon_smtp_host_obj = create_custom_field("smtpHostname",
-                                                         smtp_host)
-            bitwarden.create_login(name='mastodon-smtp-credentials',
-                                   item_url=mastodon_hostname,
-                                   user=smtp_user,
-                                   password=smtp_pass,
-                                   fields=[mastodon_smtp_host_obj])
+            mastodon_smtp_host_obj = create_custom_field("smtpHostname", smtp_host)
+            smtp_id = bitwarden.create_login(
+                    name='mastodon-smtp-credentials',
+                    item_url=mastodon_hostname,
+                    user=smtp_user,
+                    password=smtp_pass,
+                    fields=[mastodon_smtp_host_obj]
+                    )
 
             # S3 credentials
-            mastodon_s3_host_obj = create_custom_field("s3Endpoint",
-                                                       s3_endpoint)
-            mastodon_s3_bucket_obj = create_custom_field("s3Bucket",
-                                                         s3_bucket)
-            bitwarden.create_login(name='mastodon-s3-credentials',
-                                   item_url=mastodon_hostname,
-                                   user=s3_access_id,
-                                   password=s3_access_key,
-                                   fields=[mastodon_s3_host_obj,
-                                           mastodon_s3_bucket_obj])
+            mastodon_s3_host_obj = create_custom_field("s3Endpoint", s3_endpoint)
+            mastodon_s3_bucket_obj = create_custom_field("s3Bucket", s3_bucket)
+            s3_id = bitwarden.create_login(
+                    name='mastodon-s3-credentials',
+                    item_url=mastodon_hostname,
+                    user=s3_access_id,
+                    password=s3_access_key,
+                    fields=[mastodon_s3_host_obj,
+                    mastodon_s3_bucket_obj]
+                    )
+
+            # update the mastodon values for the argocd appset
+            fields = {
+                    'mastodon_admin_credentials_bitwarden_id': admin_id,
+                    'mastodon_smtp_credentials_bitwarden_id': smtp_id,
+                    'mastodon_postgres_credentials_bitwarden_id': db_id,
+                    'mastodon_redis_bitwarden_id': redis_id,
+                    'mastodon_s3_credentials_bitwarden_id': s3_id,
+                    }
+            update_secret_key(k8s_obj, 'appset-secret-vars', 'argocd', fields,
+                              'secret_vars.yaml')
+
+            # reload the argocd appset secret plugin
+            try:
+                k8s_obj.reload_deployment('argocd-appset-secret-plugin', 'argocd')
+            except Exception as e:
+                log.error(
+                        "Couldn't scale down the "
+                        "[magenta]argocd-appset-secret-plugin[/] deployment "
+                        f"in [green]argocd[/] namespace. Recieved: {e}"
+                        )
+
+            # reload the bitwarden ESO provider
+            try:
+                k8s_obj.reload_deployment('bitwarden-eso-provider', 'external-secrets')
+            except Exception as e:
+                log.error(
+                        "Couldn't scale down the [magenta]bitwarden-eso-provider"
+                        "[/] deployment in [green]external-secrets[/] namespace."
+                        f"Recieved: {e}"
+                        )
 
         # these are standard k8s secrets yaml
         else:
--- smol_k8s_lab/k8s_apps/social/matrix.py
+++ smol_k8s_lab/k8s_apps/social/matrix.py
@@ -1,9 +1,11 @@
 from smol_k8s_lab.bitwarden.bw_cli import BwCLI, create_custom_field
 from smol_k8s_lab.k8s_tools.argocd_util import install_with_argocd
 from smol_k8s_lab.k8s_tools.k8s_lib import K8s
+from smol_k8s_lab.k8s_tools.kubernetes_util import update_secret_key
 from smol_k8s_lab.utils.rich_cli.console_logging import sub_header, header
 from smol_k8s_lab.utils.passwords import create_password
 
+import logging as log
 
 def configure_matrix(k8s_obj: K8s,
                      config_dict: dict,
@@ -28,30 +30,76 @@ def configure_matrix(k8s_obj: K8s,
         if bitwarden:
             sub_header("Creating secrets in Bitwarden")
 
-            # SMTP credentials
+            # postgresql credentials
             matrix_pgsql_password = bitwarden.generate()
             postgres_hostname = create_custom_field("hostname",
                                                     'matrix-web-app-postgresql')
-            bitwarden.create_login(
+            db_id = bitwarden.create_login(
                     name='matrix-pgsql-credentials',
                     item_url=matrix_hostname,
                     user='matrix',
                     password=matrix_pgsql_password,
-                    fields=[postgres_hostname])
+                    fields=[postgres_hostname]
+                    )
 
             # SMTP credentials
             matrix_smtp_host_obj = create_custom_field("smtpHostname", smtp_host)
-            bitwarden.create_login(name='mastodon-smtp-credentials',
-                                   item_url=matrix_hostname,
-                                   user=smtp_user,
-                                   password=smtp_pass,
-                                   fields=[matrix_smtp_host_obj])
+            smtp_id = bitwarden.create_login(
+                    name='mastodon-smtp-credentials',
+                    item_url=matrix_hostname,
+                    user=smtp_user,
+                    password=smtp_pass,
+                    fields=[matrix_smtp_host_obj]
+                    )
+
+            # registration key
+            matrix_registration_key = bitwarden.generate()
+            reg_id = bitwarden.create_login(
+                    name='mastodon-registration-key',
+                    item_url=matrix_hostname,
+                    user="admin",
+                    password=matrix_registration_key
+                    )
+
+            # update the matrix values for the argocd appset
+            fields = {
+                    'matrix_oidc_credentials_bitwarden_id': "not-set-yet",
+                    'matrix_registration_credentials_bitwarden_id': reg_id,
+                    'matrix_smtp_credentials_bitwarden_id': smtp_id,
+                    'matrix_postgres_credentials_bitwarden_id': db_id,
+                    }
+            update_secret_key(k8s_obj, 'appset-secret-vars', 'argocd', fields,
+                              'secret_vars.yaml')
+
+            # reload the argocd appset secret plugin
+            try:
+                k8s_obj.reload_deployment('argocd-appset-secret-plugin', 'argocd')
+            except Exception as e:
+                log.error(
+                        "Couldn't scale down the "
+                        "[magenta]argocd-appset-secret-plugin[/]"
+                        "deployment in [green]argocd[/] namespace. Recieved: "
+                        f"{e}"
+                        )
+
+            # reload the bitwarden ESO provider
+            try:
+                k8s_obj.reload_deployment('bitwarden-eso-provider', 'external-secrets')
+            except Exception as e:
+                log.error(
+                        "Couldn't scale down the [magenta]bitwarden-eso-provider[/]"
+                        "deployment in [green]external-secrets[/] namespace. Recieved: "
+                        f"{e}"
+                        )
 
         # else create these as Kubernetes secrets
         else:
             matrix_pgsql_password = create_password()
             k8s_obj.create_secret('matrix-pgsql-credentials', 'matrix',
                                   {"password": matrix_pgsql_password})
+            matrix_registration_key = create_password()
+            k8s_obj.create_secret('matrix-registration', 'matrix',
+                                  {"registrationSharedSecret": matrix_registration_key})
 
     install_with_argocd(k8s_obj, 'matrix', config_dict['argo'])
     return True
