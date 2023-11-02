500d065cf841dc038a2e7f30ee08b6b15c99397b
jessebot <jessebot@linux.com>
2023-11-02 09:39:37 +0100

    make it so that if no explicit version is passed into the helm obj, we grab the live version in the argocd-apps repo

--- smol_k8s_lab/k8s_tools/helm.py
+++ smol_k8s_lab/k8s_tools/helm.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python3
 """
-AUTHOR: @jessebot email: jessebot(AT)linux(d0t)com
+AUTHOR: @jessebot
+LICENSE: AGPLv4
 """
-from collections import OrderedDict
-import logging as log
-from shutil import which
+
+# internal libraries
 from ..utils.subproc import subproc
 from ..utils.rich_cli.console_logging import header, sub_header
+
+# external libraries
+from collections import OrderedDict
+import logging as log
+import requests
+from ruamel.yaml import YAML
+from shutil import which
+
+# these are the URLs of each manually installed helm chart, so that the appset matches
+APPSET_URLS = {
+        "argocd": "https://raw.githubusercontent.com/small-hack/argocd-apps/main/argocd/argocd_argocd_appset.yaml",
+        "appset-secret-plugin": "https://raw.githubusercontent.com/small-hack/argocd-apps/main/argocd/appset_secret_plugin_generator_argocd_app.yaml",
+        "cert-manager": "https://raw.githubusercontent.com/small-hack/argocd-apps/main/cert-manager/cert-manager_argocd_app.yaml",
+        "metallb": "https://raw.githubusercontent.com/small-hack/argocd-apps/main/argocd/metallb/metallb_argocd_app.yaml",
+        "ingress-nginx": "https://raw.githubusercontent.com/small-hack/argocd-apps/main/ingress-nginx/ingress-nginx_argocd_app.yaml",
+        "cilium": "https://raw.githubusercontent.com/small-hack/argocd-apps/main/alpha/cilium/cilium_argocd_appset.yaml"
+        }
 
 
 class Helm:
@@ -101,6 +118,9 @@ class Helm:
 
             if self.__dict__.get('chart_version', False):
                 cmd += f' --version {self.chart_version}'
+            else:
+                version = self.get_appset_version()
+                cmd += f' --version {version}'
 
             if self.__dict__.get('values_file', False):
                 cmd += f' --values {self.values_file}'
@@ -114,6 +134,14 @@ class Helm:
 
             subproc([cmd])
             return True
+
+        def get_appset_version(self) -> str:
+            """
+            go get the version of the helm chart installed by the live appset
+            """
+            yaml = YAML()
+            obj = yaml.load(requests.get(APPSET_URLS[self.release_name]))
+            return obj['spec']['template']['spec']['source']['targetRevision']
 
         def uninstall(self):
             """
