64b9b993b65245502d8e98eef67180745619ecb2
jessebot <jessebot@linux.com>
2023-12-02 19:46:17 +0100

    add more test screenshots

--- smol_k8s_lab/tui/make_screenshots.py
+++ smol_k8s_lab/tui/make_screenshots.py
@@ -10,8 +10,16 @@ async def make_screenshots() -> None:
     """
     make all the screenshots
     """
-    async with BaseApp().run_test(size=(87, 48)) as pilot:
+    async with BaseApp().run_test(size=(86, 47)) as pilot:
         pilot.app.save_screenshot("./docs/images/screenshots/start_screen.svg")
+
+        # Test pressing the "tab" key followed by the "c" key
+        await pilot.press("tab", "c")  
+        pilot.app.save_screenshot("./docs/images/screenshots/tui_config_screen.svg")
+
+        # Test pressing the "q" key
+        await pilot.press("q", "h")  
+        pilot.app.save_screenshot("./docs/images/screenshots/tui_help_screen.svg")
 
 if __name__ == "__main__":
     asyncio.run(make_screenshots())
