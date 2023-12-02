7d996f582d8c15c690a623e55fadea040ffe65bc
jessebot <jessebot@linux.com>
2023-12-02 19:31:49 +0100

    add more testing tools and test start screen screenshot again

--- smol_k8s_lab/tui/make_screenshots.py
+++ smol_k8s_lab/tui/make_screenshots.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
 # shamelessly stolen from:
 # https://blog.davep.org/2023/07/03/making-my-github-banner.html
+
 from smol_k8s_lab.tui.base import BaseApp
 import asyncio
 
@@ -8,7 +10,7 @@ async def make_screenshots() -> None:
     """
     make all the screenshots
     """
-    async with BaseApp().run_test() as pilot:
+    async with BaseApp().run_test(size=(89, 100)) as pilot:
         pilot.app.save_screenshot("./docs/images/screenshots/start_screen.svg")
 
 if __name__ == "__main__":
