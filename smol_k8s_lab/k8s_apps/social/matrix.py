626d6ab809b3c18616e3a6ee0360d443ea27d6b0
jessebot <jessebot@linux.com>
2024-04-28 11:30:29 +0200

    announce we're syncing matrix when we do

--- smol_k8s_lab/k8s_apps/social/matrix.py
+++ smol_k8s_lab/k8s_apps/social/matrix.py
@@ -30,7 +30,10 @@ def configure_matrix(k8s_obj: K8s,
 
     init_enabled = config_dict['init']['enabled']
     # initial secrets to deploy this app from scratch
-    if init_enabled and not app_installed:
+    if app_installed:
+        header("syncing [green]Matrix[/], so you can self host your own chat",
+               '💬')
+    elif init_enabled and not app_installed:
         init_values = config_dict['init']['values']
 
         restore_dict = config_dict['init'].get('restore', {"enabled": False})
