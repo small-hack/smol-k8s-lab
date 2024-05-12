66b40f55dc4f44553d6950103435b8efd49fdc46
jessebot <jessebot@linux.com>
2024-05-12 11:47:58 +0200

    fix option evaluation for final run command

--- smol_k8s_lab/utils/run/final_cmd.py
+++ smol_k8s_lab/utils/run/final_cmd.py
@@ -1,4 +1,5 @@
 from .subproc import subproc
+from smol_k8s_lab.utils.rich_cli.console_logging import header, sub_header
 
 
 def run_final_cmd(command: str = 'applications.argoproj.io',
@@ -8,6 +9,11 @@ def run_final_cmd(command: str = 'applications.argoproj.io',
     runs k9s
     takes one command str to run when k9s is launched
     """
+    header("Running post-config command", "💻")
+    sub_header(f"Running command: '{command}'")
+    sub_header(f"terminal: '{terminal}'")
+    sub_header(f"window_behavior: '{window_behavior}'")
+
     if not window_behavior:
         subproc([command], spinner=False)
     else:
@@ -25,25 +31,25 @@ def run_zellij_command(command: str, window_behavior: str = ""):
     if not window_behavior:
         cmd = command
 
-    elif window_behavior == "split-right":
+    elif window_behavior == "split right":
         cmd = f"zellij run --direction right -- {command}"
 
-    elif window_behavior == "split-left":
+    elif window_behavior == "split left":
         cmd = f"zellij run --direction left -- {command}"
 
-    elif window_behavior == "split-top":
+    elif window_behavior == "split top":
         cmd = f"zellij run --direction up -- {command}"
 
-    elif window_behavior == "split-bottom":
+    elif window_behavior == "split bottom":
         cmd = f"zellij run --direction down -- {command}"
 
-    elif window_behavior == "new-tab" or window_behavior == "new-window":
+    elif window_behavior == "new tab" or window_behavior == "new-window":
         cmd = f"zellij run --floating -- {command}"
 
-    elif window_behavior == "new-window":
+    elif window_behavior == "new window":
         cmd = f"zellij run --floating --  {command}"
 
-    subproc([cmd], spinner=False)
+    subproc([cmd], spinner=False, quiet=True)
 
 
 def run_wezterm_command(command: str, window_behavior: str = ""):
@@ -53,22 +59,22 @@ def run_wezterm_command(command: str, window_behavior: str = ""):
     if not window_behavior:
         cmd = command
 
-    elif window_behavior == "split-right":
+    elif window_behavior == "split right":
         cmd = f"wezterm cli split-pane --right {command}"
 
-    elif window_behavior == "split-left":
+    elif window_behavior == "split left":
         cmd = f"wezterm cli split-pane --left {command}"
 
-    elif window_behavior == "split-top":
+    elif window_behavior == "split top":
         cmd = f"wezterm cli split-pane --top {command}"
 
-    elif window_behavior == "split-bottom":
+    elif window_behavior == "split bottom":
         cmd = f"wezterm cli split-pane --bottom {command}"
 
-    elif window_behavior == "new-tab":
+    elif window_behavior == "new tab":
         cmd = f"wezterm cli spawn {command}"
 
-    elif window_behavior == "new-window":
+    elif window_behavior == "new window":
         cmd = f"wezterm cli spawn --new-window {command}"
 
-    subproc([cmd], spinner=False)
+    subproc([cmd], spinner=False, quiet=True)
