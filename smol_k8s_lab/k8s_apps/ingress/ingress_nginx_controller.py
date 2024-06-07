b2e3b1cbc14d61dcf600f5240f519fc030eeb5dd
jessebot <jessebot@linux.com>
2024-06-07 13:06:59 +0200

    update ingress-nginx install to include some of the defaults from the upstream argocd app

--- smol_k8s_lab/k8s_apps/ingress/ingress_nginx_controller.py
+++ smol_k8s_lab/k8s_apps/ingress/ingress_nginx_controller.py
@@ -5,9 +5,14 @@ DESCRIPTION: install the nginx ingress controller
      AUTHOR: @jessebot
     LICENSE: GNU AFFERO GENERAL PUBLIC LICENSE Version 3
 """
+# external libraries
+from os import path
+import yaml
+
 # internal libraries
 from smol_k8s_lab.k8s_tools.helm import Helm
 from smol_k8s_lab.k8s_tools.k8s_lib import K8s
+from smol_k8s_lab.constants import XDG_CACHE_DIR
 
 
 def configure_ingress_nginx(k8s_obj: K8s, k8s_distro: str) -> None:
@@ -26,9 +31,36 @@ def configure_ingress_nginx(k8s_obj: K8s, k8s_distro: str) -> None:
                 selector="app.kubernetes.io/component=controller"
                 )
     else:
-        values = {"controller.allowSnippetAnnotations": True}
+        values = {
+                "controller": {
+                    "configAnnotations": {
+                        "allow-snippet-annotations": "true",
+                        "forwarded-for-header": "X-Forwarded-For",
+                        "proxy-real-ip-cidr": "0.0.0.0/0",
+                        "real-ip-header": "X-Real-IP",
+                        "use-forwarded-headers": "true"
+                    },
+                    "allowSnippetAnnotations": True,
+                    "resources": {
+                        "requests": {
+                            "cpu": "100m",
+                            "memory": "90Mi"
+                            }
+                    },
+                    "service": {
+                        "enabled": True,
+                        "external": {"enabled": True},
+                        "type": "LoadBalancer",
+                        "externalTrafficPolicy": "Local"
+                    }
+                }
+        }
+        values_file_name = path.join(XDG_CACHE_DIR, 'ingres_nginx_values.yaml')
+        with open(values_file_name, 'w') as values_file:
+            yaml.dump(values, values_file)
+
         release = Helm.chart(release_name='ingress-nginx',
                              chart_name='ingress-nginx/ingress-nginx',
                              namespace='ingress-nginx',
-                             set_options=values)
-        release.install()
+                             values_file=values_file_name)
+        release.install(wait=True)
