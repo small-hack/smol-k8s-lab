b7944347204d55cbeb5a8807fd3a4254c6187ef2
jessebot <jessebot@linux.com>
2025-08-24 14:18:55 +0200

    start setting up the bitwarden credentials

--- smol_k8s_lab/k8s_apps/special_sauce/install_custom_app.py
+++ smol_k8s_lab/k8s_apps/special_sauce/install_custom_app.py
@@ -41,13 +41,23 @@ class install_custom_app():
             bitwarden: BwCLI = BwCLI("test","test","test")
                  ) -> None:
         self.argocd = argocd
-        self.app_name = app_cfg["name"]
-        self.oidc_creds = {}
         self.zitadel = zitadel
+        self.bitwarden = bitwarden
+        # name of the app to install, used basically everywhere
+        self.app_name = app_cfg["name"]
+        self.app_hostname = app_cfg["hostname"]
         # check if init is even enabled
         self.init_enabled = app_cfg['init']['enabled']
         # check if we need to setup oidc
         self.oidc_cfg = app_cfg['init'].get('oidc', {'enabled': False})
+        # placeholder for oidc credentials
+        self.oidc_creds = {}
+        # check if we need to setup a self-hosted S3
+        self.s3_cfg = app_cfg['init'].get('s3', {'enabled': False})
+        # check if we need to setup a postgresql database
+        self.pgsql_cfg = app_cfg['init'].get('postgresql', {'enabled': False})
+        # check if we need to setup a valkey cache
+        self.valkey_cfg = app_cfg['init'].get('valkey', {'enabled': False})
         # this can be setup, restore, or sync
         self.app_phase = "setup"
 
@@ -58,6 +68,9 @@ class install_custom_app():
             # setup an oidc app via zitadel if required
             if self.oidc_cfg['enabled']:
                 self.setup_oidc(self)
+
+            if bitwarden:
+                self.setup_bitwarden_secrets()
 
     def setup_oidc(self) -> True:
         """
@@ -96,15 +109,65 @@ class install_custom_app():
 
     def setup_s3(self) -> True:
         """
-        setup minio alias
+        setup S3 credentails and minio alias locally
         """
+        s3_endpoint = self.s3_cfg['endpoint']
+
+        # choose a random password for the S3 access key secret
         s3_access_key = create_password()
+
         # create a local alias to check and make sure nextcloud is functional
         create_minio_alias(minio_alias=self.app_name,
                            minio_hostname=s3_endpoint,
                            access_key=self.app_name,
                            secret_key=s3_access_key)
         return True
+
+
+    def setup_bitwarden_secrets(self) -> True:
+        """
+        setup bitwarden and/or openbao secrets
+        """
+        hostname = self.app_hostname
+        bw_id_dict = {}
+
+        if self.s3_cfg['enabled']:
+            s3_endpoint = self.s3_cfg['endpoint']
+
+            # s3 credentials creation
+            bucket_obj = create_custom_field('bucket', f'{self.app_name}-data')
+            s3_access_key = create_password()
+            endpoint_obj = create_custom_field('endpoint', s3_endpoint)
+            s3_id = self.bitwarden.create_login(
+                    name=f'{self.app_name}-user-s3-credentials',
+                    item_url=hostname,
+                    user=self.app_name,
+                    password=s3_access_key,
+                    fields=[bucket_obj, endpoint_obj]
+                    )
+            bw_id_dict[f'{self.app_name}_credentials_bitwarden_id'] = s3_id
+
+            # TODO: Do we actually need this?
+            admin_s3_key = create_password()
+            s3_admin_id = self.bitwarden.create_login(
+                    name=f'{self.app_name}-admin-s3-credentials',
+                    item_url=hostname,
+                    user=f"{self.app_name}-root",
+                    password=admin_s3_key
+                    )
+            bw_id_dict[f'{self.app_name}_s3_admin_credentials_bitwarden_id'] = s3_admin_id
+
+            # if we're also setting up postgresql, make sure it can backup to s3
+            if self.pgsql_cfg['enabled']:
+                pgsql_s3_key = create_password()
+                s3_db_id = self.bitwarden.create_login(
+                        name=f'{self.app_name}-postgres-s3-credentials',
+                        item_url=hostname,
+                        user=f"{self.app_name}-postgres",
+                        password=pgsql_s3_key
+                        )
+                bw_id_dict[f'{self.app_name}_s3_postgres_credentials_bitwarden_id'] = s3_db_id
+
 
     def restore_app(self) -> True:
         """
